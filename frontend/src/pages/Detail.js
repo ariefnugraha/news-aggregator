64814f1bf33e2dd38f48a7aca903e7960a0723f8
Arief <muhammadariefnugraha77@gmail.com>
2021-06-13 21:55:52 +0700

    add script to detect scroll to change navbar

--- frontend/src/pages/Detail.js
+++ frontend/src/pages/Detail.js
@@ -1,6 +1,30 @@
+import {useState, useEffect} from 'react';
+import style from './css/detail.module.css';
+
+import Navbar from '../components/Navbar';
+import NavbarDetail from '../components/NavbarDetail';
+
+
 const Detail = () => {
+    const [scrollPosition, setscrollPosition] = useState(0);
+    const handleScroll = () => {
+        console.log(window.scrollY);
+        setscrollPosition(window.scrollY);
+    }
+    let renderNavbar;
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [scrollPosition]);
+
+    scrollPosition > 200 ? renderNavbar = <NavbarDetail /> : renderNavbar = <Navbar />
+
     return (
-        <h1>Detail</h1>
+        <div className={style.detail}>
+            {renderNavbar}
+        </div>
+       
     )
 }
 
