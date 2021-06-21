db0eff2bfeb19df2715e85cb7fa31afee453419f
Arief <muhammadariefnugraha77@gmail.com>
2021-06-21 22:08:24 +0700

    add route for detail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import Detail from './pages/Detail';
 
 function App() {
   return (
-      <Router>f
+      <Router>
         <Switch>
           <Route path="/" exact component={Homepage} />
+          <Route path="/detail" component={Detail} />
         </Switch>
       </Router>
   );
