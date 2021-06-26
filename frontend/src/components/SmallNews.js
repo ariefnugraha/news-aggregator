b3e035e7fd2abc30216fa516acc945c0231f7c60
Arief <muhammadariefnugraha77@gmail.com>
2021-06-26 10:02:40 +0700

    make component small news

---
+++ frontend/src/components/SmallNews.js
@@ -0,0 +1,21 @@
+import style from './css/smallnews.module.css';
+import {Link} from 'react-router-dom';
+
+const NewsList = () => {
+    return (
+        <div className={style.small_news}>
+            <div className={style.image}>
+                <img src="https://mojok.co/wp-content/uploads/2019/06/menangis-laki-laki.jpg" alt="TITLE" />
+            </div>
+            <div className={style.caption}>
+                <h2>
+                    <Link to="">Asahi daily, an official Tokyo Olympics partner, calls for cancellation of Games</Link>
+                </h2>
+                <Link to="#" className={style.category}>General</Link>
+                <span>12 January 2020</span>
+            </div>
+        </div>
+    )
+}
+
+export default NewsList;
