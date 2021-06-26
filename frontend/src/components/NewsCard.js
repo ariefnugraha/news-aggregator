0463cbf2438fc9c79efae1797ac5167c1926e327
Arief <muhammadariefnugraha77@gmail.com>
2021-06-26 10:06:07 +0700

    add h2 tag

--- frontend/src/components/NewsCard.js
+++ frontend/src/components/NewsCard.js
@@ -1,15 +1,17 @@
 import style from './css/newscard.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const NewsCard = (props) => {
     return (
         <div className={style.news_card} style={props.width}>
             <div className={style.news_image}>
                 <Link to="#">
-                <img src="https://mojok.co/wp-content/uploads/2019/06/menangis-laki-laki.jpg" alt="TITLE" />
+                    <img src="https://mojok.co/wp-content/uploads/2019/06/menangis-laki-laki.jpg" alt="TITLE" />
                 </Link>
             </div>
             <div className={style.news_caption}>
-                <Link to="">Indonesia’s beloved pop/rock band Naif officially disbands after 25 years</Link>
+                <h2>
+                    <Link to="">Indonesia’s beloved pop/rock band Naif officially disbands after 25 years</Link>
+                </h2>
                 <p>Indonesia’s beloved pop/rock band Naif officially disbands after 25 years</p>
             </div>
         </div>
