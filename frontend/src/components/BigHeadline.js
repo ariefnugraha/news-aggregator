4369bca90c937078679527fb8f250143f35399ad
Arief <muhammadariefnugraha77@gmail.com>
2021-07-25 21:31:36 +0700

    add url to link

--- frontend/src/components/BigHeadline.js
+++ frontend/src/components/BigHeadline.js
@@ -1,17 +1,25 @@
 import style from './css/bigheadline.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const BigHeadline = () => {
+const BigHeadline = ({ news }) => {
     return (
         <div className={style.big_headline}>
             <h1>
-                <Link to="" title="Warga Palestina yang Terancam Digusur Israel di Yerusalem Masih Menunggu Kepastian">Warga Palestina yang Terancam Digusur Israel di Yerusalem Masih Menunggu Kepastian</Link>
+                <Link to={{
+                    pathname: "/detail",
+                    search: `${news.title}`,
+                    state: { newsTitle: news.title }
+                }} title={news.title}>{news.title}</Link>
             </h1>
-            <p>Proses pengadilan empat keluarga Palestina inilah yang memicu konflik besar hingga merembet ke Jalur Gaza. Rumah mereka terancam diambil paksa untuk permukiman Yahudi.</p>
+            <p>{news.description}.</p>
             <div className={style.image_container}>
-                <Link to="/">
-                    <img src="https://video-images.vice.com/articles/609eafb6344058009b8d01e0/lede/1621013114379-israelphoto-6.jpeg?crop=1xw:0.8426xh;0xw,0.0639xh&resize=711:*" alt="Warga Palestina yang Terancam Digusur Israel di Yerusalem Masih Menunggu Kepastian" />
-                    </Link>    
+                <Link to={{
+                    pathname: "/detail",
+                    search: `${news.title}`,
+                    state: { newsTitle: news.title }
+                }}>
+                    <img src={news.image} alt={news.title} />
+                </Link>
             </div>
         </div>
     )
