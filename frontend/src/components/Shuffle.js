cb6bb1971c791887100ba3467e0b9be3f07e85a4
Arief <muhammadariefnugraha77@gmail.com>
2021-07-27 11:41:54 +0700

    make content dynamic based on props

--- frontend/src/components/Shuffle.js
+++ frontend/src/components/Shuffle.js
@@ -1,36 +1,69 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import style from './css/shuffle.module.css';
-
 import SmallNews from './SmallNews';
 
-const Shuffle = () => {
-    return (
-        <div className={style.shuffle_container}>
-            <div className={style.big_news_container}>
-                <div className={style.news_image}>
-                    <img src="https://mojok.co/wp-content/uploads/2019/06/menangis-laki-laki.jpg" alt="TITLE" />
-                </div>
-                <div className={style.news_caption}>
-                    <h2>
-                        <Link to="#">'Epic Iran' puts 5,000 years of artefacts on show at London's V&A</Link>
-                    </h2>
+const Shuffle = ({ newsId }) => {
+    const [news, setnews] = useState([{
+        id: 0,
+        image: "https://akcdn.detik.net.id/community/media/visual/2017/09/14/024a4104-dc93-4f94-920c-fedf21be10b0_169.jpg?w=620",
+        title: " ",
+        category: ' ',
+        date: ' '
+    }]);
 
-                    <p>From ancient sculptures to contemporary photographs, objects spanning 5,000 years of Iranian art, culture and design go on display at London's Victoria & Albert (V&A) museum this week.</p>
+    useEffect(() => {
+        axios.get("http://localhost/news/backend/api/get/shuffle.php", {
+            params: { newsId }
+        })
+            .then(response => setnews(response.data))
+            .catch(error => console.log(error));
+    }, []);
 
-                    <div className={style.info}>
-                        <Link to="#">General</Link>
-                        <span>11 January 2020</span>
+    if (news === undefined || news === null) {
+        console.log("GADADA")
+    } else {
+        let renderSmallShuffle = news.slice(1, news.length).map((news) => {
+            return (
+                <SmallNews key={news.id} image={news.image} title={news.title} category={news.category} date={news.date} />
+            )
+        })
+
+        return (
+            <div className={style.shuffle_container}>
+                <div className={style.big_news_container}>
+                    <div className={style.news_image}>
+                        <img src={news[0].image} alt={news[0].title} />
+                    </div>
+                    <div className={style.news_caption}>
+                        <h2>
+                            <Link to={{
+                                pathname: "/detail",
+                                search: `${news[0].title}`,
+                                state: { newsTitle: news[0].title }
+                            }}>{news[0].title}</Link>
+                        </h2>
+
+                        <div className={style.info}>
+                            <Link to={{
+                                pathname: "/category",
+                                search: news[0].title,
+                                state: { category: news[0].category }
+                            }}>{news[0].category}</Link>
+                            <span>{news[0].date}</span>
+                        </div>
                     </div>
                 </div>
+                <div className={style.small_news_container}>
+                    {renderSmallShuffle}
+                </div>
             </div>
-            <div className={style.small_news_container}>
-                <SmallNews />
-                <SmallNews />
-                <SmallNews />
-                <SmallNews />
-            </div>
-        </div>
-    )
+        )
+    }
+
 }
+
+
 
 export default Shuffle;
