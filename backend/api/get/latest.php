bf52e6c8154025a4fe31703e03d829abfb74ebf6
Arief <muhammadariefnugraha77@gmail.com>
2021-07-29 11:07:41 +0700

    change script to provide data for latest in detail page

--- backend/api/get/latest.php
+++ backend/api/get/latest.php
@@ -7,27 +7,51 @@
     $category = "general";
     
     //$query = "SELECT id, title, description, image, category FROM articles WHERE category = ? ORDER BY id DESC LIMIT 5";
-    $query = "SELECT id, title, description, image, category, date_publish FROM articles ORDER BY id DESC";
-    $stmt = $conn->prepare($query);
-    //$stmt->bind_param('s', $category);
-    $stmt->execute();
-    $stmt->store_result();
-    $stmt->bind_result($id, $title, $description, $image, $category, $date_publish);
- 
-    if($stmt->num_rows() > 0) {
-        while($result = $stmt->fetch()) {
-            $news = array(
-                "id" => $id,
-                "title" => $title,
-                "description" => $description,
-                "image" => $image,
-                "category" => $category,
-                "date" => $date_publish
-            );
 
-            array_push($response, $news);
+    if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "SELECT id, title, description, image FROM articles WHERE id != ? ORDER BY id DESC LIMIT 5";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($id, $title, $description, $image);
+        if($stmt->num_rows() > 0) {
+            while($result = $stmt->fetch()) {
+                $news = array(
+                    "id" => $id,
+                    "title" => $title,
+                    "description" => $description,
+                    "image" => $image
+                );
+
+                array_push($response, $news);
+            }
+        }
+    } else {
+        $query = "SELECT id, title, description, image, category, date_publish FROM articles ORDER BY id DESC";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($id, $title, $description, $image, $category, $date_publish);
+        if($stmt->num_rows() > 0) {
+            while($result = $stmt->fetch()) {
+                $news = array(
+                    "id" => $id,
+                    "title" => $title,
+                    "description" => $description,
+                    "image" => $image,
+                    "category" => $category,
+                    "date" => $date_publish
+                );
+    
+                array_push($response, $news);
+            }
         }
     }
+    
+   
+ 
+    
    
     echo json_encode($response);
-?>
