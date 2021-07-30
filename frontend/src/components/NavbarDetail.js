6c570dbd6b331ea818cbbfa3c636eff03e84d266
Arief <muhammadariefnugraha77@gmail.com>
2021-07-30 14:35:40 +0700

    split title text

--- frontend/src/components/NavbarDetail.js
+++ frontend/src/components/NavbarDetail.js
@@ -1,10 +1,21 @@
 import style from './css/navbar-detail.module.css';
 import { Link } from 'react-router-dom';
+import { FacebookShareButton, TwitterShareButton, LinkedinShareButton, WhatsappShareButton, TelegramShareButton, FacebookIcon, TwitterIcon, LinkedinIcon, WhatsappIcon, TelegramIcon } from 'react-share';
 
-const NavbarDetail = (props) => {
+const NavbarDetail = ({ title, category, writer }) => {
+    let deviceWidth = (window.innerWidth > 0) ? window.innerWidth : window.screen.width;
+    let setTitle
+    if (deviceWidth > 991 && title.length > 70) {
+        setTitle = `${title.substr(0, 70)}...`;
+    } else if(deviceWidth > 767 && deviceWidth < 992 && title.length > 50){
+        setTitle = `${title.substr(0, 50)}...`;
+    } else {
+        setTitle = title;
+    }
+
     return (
         <nav className={style.navbar}>
-            <div className={style.menu}>
+            {/* <div className={style.menu}>
                 <ul>
                     <li>
                         <Link to="/">Beranda</Link>
@@ -26,24 +37,43 @@ const NavbarDetail = (props) => {
                     </li>
                 </ul>
                 <form>
-                    <input type="text" placeholder="Apa Yang Ingin Kamu Ketahui?" required />
+                    <input type="text" placeholder="Cari Berita" required />
                     <button></button>
                 </form>
-            </div>
+            </div> */}
+          
             <div className={style.nav_title}>
                 <div className={style.back_container}>
                     <Link to="/"></Link>
                 </div>
                 <div className={style.main_title}>
-                    <p>Indonesia calls for international support to resolve Israel-Palestine crisis</p>
-                    <p>by <Link to="">Alexander Christian Hutabarat</Link> in <Link to="">General</Link></p>
+                    <p>{setTitle}</p>
+                    <p>oleh <Link to="">{writer}</Link> di <Link to="">{category}</Link></p>
                 </div>
                 <div className={style.share}>
                     <p>Share</p>
-                    <Link to="/">adad</Link>
+                    <FacebookShareButton url={title}>
+                        <FacebookIcon size={35} iconFillColor={'black'} bgStyle={{fill: "#fff"}}/>
+                    </FacebookShareButton>
+
+                    <TwitterShareButton url={title}>
+                        <TwitterIcon size={35} iconFillColor={'black'} bgStyle={{fill: "#fff"}}/>
+                    </TwitterShareButton>
+
+                    <WhatsappShareButton url={title}>
+                        <WhatsappIcon size={35} iconFillColor={'black'} bgStyle={{fill: "#fff"}} />
+                    </WhatsappShareButton>
+
+                    <TelegramShareButton url={title}>
+                        <TelegramIcon size={35} iconFillColor={'black'} bgStyle={{fill: "#fff"}} />
+                    </TelegramShareButton>
+
+                    <LinkedinShareButton url={title}>
+                        <LinkedinIcon size={35} iconFillColor={'black'} bgStyle={{fill: "#fff"}} />
+                    </LinkedinShareButton>
                 </div>
             </div>
-            <hr/>
+            <hr />
         </nav>
     )
 }
